567fd97a17a465135a0c64377fbd7fe60c835245
dscx <dnchmbrln@gmail.com>
2019-03-20 12:35:53 -0700

    update template

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,8 +1,30 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-const Template = props => {
-  return <div>Blog post here</div>
+const Template = ({ data }) => {
+  const {
+    markdownRemark: {
+      html,
+      frontmatter: { title },
+    },
+  } = data
+  return (
+    <div>
+      <h1 style={{ fontFamily: "avenir" }}>{title}</h1>
+      <div className="blogpost" dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  )
 }
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
 
 export default Template
