7a320fbc63841074c0d47f60eb1c513af21152b2
dscx <dnchmbrln@gmail.com>
2019-03-20 12:51:30 -0700

    add next/prev links

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path")
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
@@ -8,7 +8,9 @@ exports.createPages = ({ graphql, actions }) => {
       graphql(
         `
           query {
-            allMarkdownRemark {
+            allMarkdownRemark(
+              sort: { order: ASC, fields: [frontmatter___date] }
+            ) {
               edges {
                 node {
                   frontmatter {
@@ -20,13 +22,16 @@ exports.createPages = ({ graphql, actions }) => {
           }
         `
       ).then(result => {
-        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        const posts = result.data.allMarkdownRemark.edges
+        posts.forEach(({ node }, index) => {
           const path = node.frontmatter.path
           createPage({
             path,
             component: blogPostTemplate,
             context: {
               pathSlug: path,
+              prev: index === 0 ? null : posts[index - 1].node,
+              next: index === posts.length - 1 ? null : posts[index + 1].node,
             },
           })
           resolve()
--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
-const Template = ({ data }) => {
+const Template = ({ data, pageContext }) => {
+  const { next, prev } = pageContext
   const {
     markdownRemark: {
       html,
@@ -12,6 +13,12 @@ const Template = ({ data }) => {
     <div>
       <h1 style={{ fontFamily: "avenir" }}>{title}</h1>
       <div className="blogpost" dangerouslySetInnerHTML={{ __html: html }} />
+      <div style={{ marginBottom: "1rem", fontFamily: "avenir" }}>
+        {next && <Link to={next.frontmatter.path}>Next</Link>}
+      </div>
+      <div style={{ fontFamily: "avenir" }}>
+        {prev && <Link to={prev.frontmatter.path}>Prev</Link>}
+      </div>
     </div>
   )
 }
